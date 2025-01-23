1620b194e8e47054f9a0ad2c5b461d7c646dd8e7
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-01-23 13:34:13 -0300

    finalizando

--- GeradorSenhas/obj/Debug/net9.0/GeradorSenhas.AssemblyInfo.cs
+++ GeradorSenhas/obj/Debug/net9.0/GeradorSenhas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeradorSenhas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f89040d5275ea9515993a1038142c2da0d1e6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c8550da4ed2bceb11e4d904728487dada7daccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeradorSenhas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeradorSenhas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
